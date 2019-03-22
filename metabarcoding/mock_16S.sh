48e418d672c131815f2181d1cf1722c3ac1a64fa
MGalati <46815903+MGalati@users.noreply.github.com>
2019-03-22 11:10:13 +0100

    Update mock_16S.sh

--- metabarcoding/mock_16S.sh
+++ metabarcoding/mock_16S.sh
@@ -7,28 +7,20 @@
 #$ -V
 #$ -cwd
 
-echo "Activation du module conda"
-module purge
-module load system/conda/5.1.0
-
-echo "Activation de l'environnement conda"
-conda activate qiime2-2018.11
-
 MOCK_S=/homedir/galati/mock/analysis/16S/single/
 MOCK_P=/homedir/galati/mock/analysis/16S/pair/
 
-for i in $RAW_16S*_L001_R1_001.fastq.gz
+for i in ${RAW_16S}*_L001_R1_001.fastq.gz
 do
     prefix_16S=$(basename $i _L001_R1_001.fastq.gz) # print which sample is being processed
-ls ${MOCK_S}${prefix_16S}*_R1*.fastq* ${MOCK_S}${prefix_16S}*_R2*.fastq*
+ls ${MOCK_S}${prefix_16S}*_R1*.fastq*
 
-    echo "###Trim galore Sample" $prefix_16S "Start###"
 echo "Import des fichiers fastq.gz"
-qiime tools import \
-  --type 'SampleData[PairedEndSequencesWithQuality]' \
-  --input-path ${MOCK_S}${prefix_16S}* \
-  --input-format CasavaOneEightSingleLanePerSampleDirFmt \
-  --output-path 16S_demux-paired-end.qza
-    echo "###Trim galore Sample" $prefix_16S "DONE###"
+qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' --input-path ${MOCK_S}${i}* --input-format CasavaOneEightSingleLanePerSampleDirFmt --output-path 16S_${i}_demux-paired-end.qza
     
 done
+
+# JOB END
+date
+
+exit 0
