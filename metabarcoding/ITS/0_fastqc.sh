bad60cb9edbbfbbc5e0e1bab7dc291f0cd9bdaef
MGalati <46815903+MGalati@users.noreply.github.com>
2019-03-25 14:28:36 +0100

    Update 0_fastqc.sh

--- metabarcoding/ITS/0_fastqc.sh
+++ metabarcoding/ITS/0_fastqc.sh
@@ -12,9 +12,10 @@
 
 module purge
 module load system/conda/5.1.0
+# conda create --name fastqc_multiqc
 source activate fastqc_multiqc
 
-echo "Début des analyse FastQC/MultiQC"
+echo "Début des analyses FastQC/MultiQC"
 
 # Les fichiers doivent respecter le format Qiime2 :
 # par exemple : L2S357_15_L001_R1_001.fastq.gz
@@ -22,15 +23,15 @@ echo "Début des analyse FastQC/MultiQC"
 MOCK=/homedir/galati/mock/analysis/ITS/pair/ITS_mock24
 RAW_ITS=/homedir/galati/data/ITS/
 
-for f in MOCK RAW_ITS
+for f in ${MOCK} ${RAW_ITS}
 do
 mkdir ${f}/qc/
-fastqc -t $NSLOTS ${f}/*fastq* -o ${f}/qc/
+fastqc -t $NSLOTS ${f}/*.fastq.gz -o ${f}/qc/
 multiqc ${f}/qc/ -o ${f}/qc/
 done
 
 echo 'Fin des analyses'
 
 date
-
+ 
 exit 0
