52a6dd21214e7dfedebb4bd87f15a3e9228d1636
MGalati <46815903+MGalati@users.noreply.github.com>
2019-04-04 16:51:56 +0200

    Update 2_q2_deblur.sh

--- metabarcoding/16S/deblur/2_q2_deblur.sh
+++ metabarcoding/16S/deblur/2_q2_deblur.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#$ -q long.q
-#$ -N q2_deblur
+#$ -q normal.q
+#$ -N q2_deblur_16S
 #$ -M mathias.galati@cirad.fr
 #$ -pe parallel_smp 25
-#$ -l mem_free=12G
+#$ -l mem_free=16G
 #$ -V
 #$ -cwd
 
@@ -20,7 +20,9 @@ mv /homedir/galati/data/16S_primer_trimmed2/QC/ /homedir/galati/data/16S_primer_
 mv /homedir/galati/mock/analysis/16S/pair/Mock_S280/qc/ /homedir/galati/mock/analysis/16S/pair/Mock_S280_qc/
 mv /homedir/galati/mock/analysis/16S/pair/Mock_S280 /homedir/galati/data/
 
+rm -r deblur_output
 rm -r dada2_output
+rm -r deblur_output
 rm -r phylogeny
 rm -r taxonomy
 
@@ -46,6 +48,8 @@ qiime demux summarize \
   --verbose
 done
 
+mkdir deblur_output
+
 for seqs in ${RUN1} ${RUN2}
 do
 
@@ -53,78 +57,77 @@ do
 
 qiime quality-filter q-score \
  --i-demux ${IN}/${seqs}_demux.qza \
- --o-filtered-sequences dada2_output_${seqs}/${seqs}_demux-filtered.qza \
- --o-filter-stats dada2_output_${seqs}/${seqs}_demux-filter-stats.qza
+ --o-filtered-sequences deblur_output_${seqs}/${seqs}_demux-filtered.qza \
+ --o-filter-stats deblur_output_${seqs}/${seqs}_demux-filter-stats.qza
 
 
 qiime deblur denoise-16S \
-  --i-demultiplexed-seqs dada2_output_${seqs}/${seqs}_demux-filtered.qza \
+  --i-demultiplexed-seqs deblur_output_${seqs}/${seqs}_demux-filtered.qza \
   --p-trim-length 220 \
-  --o-representative-sequences dada2_output_${seqs}/${seqs}_rep-seqs-deblur.qza \
-  --o-table dada2_output_${seqs}/${seqs}_table-deblur.qza \
+  --o-representative-sequences deblur_output_${seqs}/${seqs}_rep-seqs-deblur.qza \
+  --o-table deblur_output_${seqs}/${seqs}_table-deblur.qza \
   --p-sample-stats \
-  --o-stats dada2_output_${seqs}/${seqs}_deblur-stats.qza
+  --o-stats deblur_output_${seqs}/${seqs}_deblur-stats.qza
 
 
 ### Viewing denoising stats
 qiime metadata tabulate \
-  --m-input-file dada2_output_${seqs}/${seqs}_demux-filter-stats.qza \
-  --o-visualization dada2_output_${seqs}/${seqs}_demux-filter-stats.qzv
+  --m-input-file deblur_output_${seqs}/${seqs}_demux-filter-stats.qza \
+  --o-visualization deblur_output_${seqs}/${seqs}_demux-filter-stats.qzv
 
 
 qiime deblur visualize-stats \
-  --i-deblur-stats dada2_output_${seqs}/${seqs}_deblur-stats.qza \
-  --o-visualization dada2_output_${seqs}/${seqs}_deblur-stats.qzv
+  --i-deblur-stats deblur_output_${seqs}/${seqs}_deblur-stats.qza \
+  --o-visualization deblur_output_${seqs}/${seqs}_deblur-stats.qzv
 
 #summarize your filtered/ASV table data
-qiime tools export --input-path dada2_output_${seqs}/${seqs}_demux-filter-stats.qza --output-path dada2_output_${seqs}/${seqs}
+qiime tools export --input-path deblur_output_${seqs}/${seqs}_demux-filter-stats.qza --output-path deblur_output_${seqs}/${seqs}
 
-qiime feature-table summarize --i-table dada2_output_${seqs}/${seqs}_table-deblur.qza --o-visualization dada2_output_${seqs}/${seqs}_table_summary.qzv --verbose
+qiime feature-table summarize --i-table deblur_output_${seqs}/${seqs}_table-deblur.qza --o-visualization deblur_output_${seqs}/${seqs}_table_summary.qzv --verbose
 
 done
 
 ### Merging denoised data
 
-mkdir dada2_output
 # ASV table
 qiime feature-table merge \
-  --i-tables dada2_output_${RUN1}/${RUN1}_table-deblur.qza \
-  --i-tables dada2_output_${RUN2}/${RUN2}_table-deblur.qza \
-  --o-merged-table dada2_output/table.qza
+  --i-tables deblur_output_${RUN1}/${RUN1}_table-deblur.qza \
+  --i-tables deblur_output_${RUN2}/${RUN2}_table-deblur.qza \
+  --o-merged-table deblur_output/table.qza
 
 # Representative sequences
 qiime feature-table merge-seqs \
-  --i-data dada2_output_${RUN1}/${RUN1}_representative_sequences.qza \
-  --i-data dada2_output_${RUN2}/${RUN2}_representative_sequences.qza \
-  --o-merged-data dada2_output/representative_sequences.qza
+  --i-data deblur_output_${RUN1}/${RUN1}_representative_sequences.qza \
+  --i-data deblur_output_${RUN2}/${RUN2}_representative_sequences.qza \
+  --o-merged-data deblur_output/representative_sequences.qza
 
 # Denoising Stats
 
-cat dada2_output_${RUN1}/${RUN1}/stats.tsv dada2_output_${RUN2}/${RUN2}/stats.tsv \
-    > dada2_output/stats.tsv
+cat deblur_output_${RUN1}/${RUN1}/stats.tsv deblur_output_${RUN2}/${RUN2}/stats.tsv \
+    > deblur_output/stats.tsv
 
 #cannot
 #qiime feature-table merge \
-#  --i-tables dada2_output/${RUN1}_denoising_stats.qza  \
-#  --i-tables dada2_output/${RUN2}_denoising_stats.qza  \
-#  --o-merged-table dada2_output/denoising_stats.qza
+#  --i-tables deblur_output/${RUN1}_denoising_stats.qza  \
+#  --i-tables deblur_output/${RUN2}_denoising_stats.qza  \
+#  --o-merged-table deblur_output/denoising_stats.qza
 
 #summarize
 qiime feature-table summarize \
-  --i-table dada2_output/table.qza \
-  --o-visualization dada2_output/table.qzv 
+  --i-table deblur_output/table.qza \
+  --o-visualization deblur_output/table.qzv 
   ##--m-sample-metadata-file sample-metadata.tsv
 
 qiime feature-table tabulate-seqs \
-  --i-data dada2_output/representative_sequences.qza\
-  --o-visualization dada2_output/representative_sequences.qzv
+  --i-data deblur_output/representative_sequences.qza\
+  --o-visualization deblur_output/representative_sequences.qzv
 
 #cannot
-#qiime feature-table summarize --i-table dada2_output/denoising_stats.qza \
-#  --o-visualization dada2_output/denoising_stats.qzv
+#qiime feature-table summarize --i-table deblur_output/denoising_stats.qza \
+#  --o-visualization deblur_output/denoising_stats.qzv
 
 #export
-#qiime tools export dada2_output/denoising_stats.qza --output-path dada2_output
+#qiime tools export deblur_output/denoising_stats.qza --output-path deblur_output
 
 #qiime feature-table summarize \
 #  --i-table table.qza \
@@ -142,15 +145,15 @@ mkdir phylogeny
 
 qiime phylogeny align-to-tree-mafft-fasttree \
   --p-n-threads ${NSLOTS} \
-  --i-sequences dada2_output/representative_sequences.qza \
+  --i-sequences deblur_output/representative_sequences.qza \
   --o-alignment phylogeny/aligned-rep-seqs.qza \
   --o-masked-alignment phylogeny/masked-aligned-rep-seqs.qza \
   --o-tree phylogeny/unrooted-tree.qza \
   --o-rooted-tree phylogeny/rooted-tree.qza \
   --verbose
 
-#qiime tools export --input-path dada2_output/deblur_table_filt.qza --output-path dada2_output_exported
-#qiime tools export --input-path dada2_output/rep_seqs_filt.qza --output-path dada2_output_exported
+#qiime tools export --input-path deblur_output/deblur_table_filt.qza --output-path deblur_output_exported
+#qiime tools export --input-path deblur_output/rep_seqs_filt.qza --output-path deblur_output_exported
 
 ### Assign Taxonomy
 # loop to test various taxonomic database - pour toi laisser juste silva123 - 
@@ -161,7 +164,7 @@ mkdir taxonomy
 
 qiime feature-classifier classify-sklearn \
   --i-classifier /homedir/galati/data/classifier/silva-132-99-nb-classifier.qza \
-  --i-reads dada2_output/representative_sequences.qza \
+  --i-reads deblur_output/representative_sequences.qza \
   --o-classification taxonomy/16S_taxonomy.qza \
   --p-n-jobs ${NSLOTS} \
   --verbose
@@ -172,7 +175,7 @@ qiime metadata tabulate \
 
 # necessite metadata
 # qiime taxa barplot \
-#  --i-table dada2_output/table.qza \
+#  --i-table deblur_output/table.qza \
 #  --i-taxonomy taxonomy/${DB}_taxonomy.qza \
 #  --o-visualization taxonomy/${DB}_taxa-bar-plots.qzv \
 #  --m-metadata-file metadata.tsv 
@@ -181,11 +184,10 @@ qiime tools export --input-path taxonomy/16S_taxonomy.qza --output-path taxonomy
 mv taxonomy/taxonomy.tsv taxonomy/16S_taxonomy.tsv
 
 
-'''
 ### Exporting and modifying BIOM tables
 
 #Creating a TSV BIOM table
-qiime tools export --input-path dada2_output/table.qza --output-path export
+qiime tools export --input-path deblur_output/table.qza --output-path export
 biom convert -i export/feature-table.biom -o export/ASV-table.biom.tsv --to-tsv
 
 cp export/ASV-table.biom.tsv export/feature-table.biom.tsv
@@ -199,38 +201,15 @@ sed -i "1d" export/ASV-table.biom.tsv
 sed -i "s/#OTU ID/#OTUID/g" export/feature-table.biom.tsv
 
 #Export Taxonomy
-# idem taxonomy
-# loop to test various taxonomic database - pour toi laisser juste silva123 - 
-# https://www.dropbox.com/s/5tckx2vhrmf3flp/silva-132-99-nb-classifier.qza?dl=0
+qiime tools export --input-path /homedir/galati/data/classifier/silva-132-99-nb-classifier.qza --output-path export
 
-for DB in silva_132_99_16S_majority_taxonomy_CCTACGGGNBGCASCAG_GACTACNVGGGTATCTAATCC_id0.85_ml350_Ml500 gg-13-8-99-nb-classifier
-#silva_132_99_16S_consensus_taxonomy_GTGCCAGCMGCCGCGGTAA_GGACTACHVGGGTWTCTAAT_id0.85_ml248_Ml257 silva_132_99_16S_consensus_taxonomy_GTGCCAGCMGCCGCGGTAA_GGACTACHVGGGTWTCTAAT_id0.8_ml210_Ml310 \
-#gg-13-8-99-515-806-nb-classifier gg-13-8-99-nb-classifier \
-#silva-132-99-515-806-nb-classifier silva-132-99-nb-classifier \
-#silva_132_99_16S_majority_taxonomy_GTGCCAGCMGCCGCGGTAA_GGACTACHVGGGTWTCTAAT_id0.85_ml248_Ml257
-do
-
-qiime tools export --input-path taxonomy/${DB}_taxonomy.qza --output-path export
-mv export/taxonomy.tsv export/${DB}_taxonomy.tsv
-#sed -i "s/Feature ID/OTUID/g" export/${DB}_taxonomy.tsv
-sed -i "1s/.*/#OTUID\ttaxonomy\tconfidence/" export/${DB}_taxonomy.tsv
-
-##Add taxonomy to biom table
-##https://forum.qiime2.org/t/exporting-and-modifying-biom-tables-e-g-adding-taxonomy-annotations/3630
-
-#cp export/taxonomy.tsv export/biom-taxonomy.tsv
-##Change the first line of biom-taxonomy.tsv (i.e. the header) to this:
-##OTUID  taxonomy  confidence
-#sed -i "1s/.*/#OTUID\ttaxonomy\tconfidence/" export/biom-taxonomy.tsv
-
-biom add-metadata -i export/ASV-table.biom.tsv  -o export/ASV-table-${DB}-taxonomy.biom \
-  --observation-metadata-fp export/${DB}_taxonomy.tsv \
+biom add-metadata -i export/ASV-table.biom.tsv  -o export/ASV-table-silva-132-taxonomy.biom \
+  --observation-metadata-fp export/silva-132_taxonomy.tsv \
   --sc-separated taxonomy
-biom convert -i export/ASV-table-${DB}-taxonomy.biom -o export/ASV-table-${DB}-taxonomy.biom.tsv --to-tsv
-done
+biom convert -i export/ASV-table-silva-132-taxonomy.biom -o export/ASV-table-silva-132-taxonomy.biom.tsv --to-tsv
 
 #Export ASV seqs
-qiime tools export --input-path dada2_output/representative_sequences.qza --output-path export
+qiime tools export --input-path deblur_output/representative_sequences.qza --output-path export
 
 #Export Tree
 qiime tools export --input-path phylogeny/unrooted-tree.qza --output-path export
@@ -242,23 +221,8 @@ mv export/tree.nwk export/rooted-tree.nwk
 # For phyloseq you will need :
 #ls export/feature-table.biom.tsv export/taxonomy.tsv export/unrooted-tree.nwk export/rooted-tree.nwk
 
-zip export/export.zip export/* dada2_outpu*/*qzv taxonomy/*.qzv
+zip export/export.zip export/* deblur_outpu*/*qzv taxonomy/*.qzv
 
-#locally
-#scp florentin2@172.28.30.116:/media/DataDrive05/Flo/ETH/export/export.zip .
-#
-#otu <- read.table(Sile = "feature-table.biom.tsv", header = TRUE)
-#tax <- read.table(Sile = "taxonomy.tsv", sep = '\t', header = TRUE)
-#merged_Sile <- merge(otu, tax, by.x = c("OTUID"), by.y=c("OTUID"))
-
-#OTU=otu_table(as.matrix(read.csv("otu_matrix.csv", sep=",", row.names=1)), taxa_are_rows = TRUE)
-#TAX=tax_table(as.matrix(read.csv("taxonomy.csv", sep=",", row.names=1)))
-#TREE = read_tree("rooted-tree.nwk")
-#Unifrac requires a rooted tree for calculation. FastUnifrac online selects an arbitrary root when an unrooted tree is uploaded.
-#https://github.com/joey711/phyloseq/issues/235
-
-#physeq = phyloseq(OTU, TAX, META, TREE)
-'''
 
 # JOB END
 date
