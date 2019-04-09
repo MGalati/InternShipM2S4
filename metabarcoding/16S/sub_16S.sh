83a875011110c8f48e230f5b81c73a54103a493d
MGalati <46815903+MGalati@users.noreply.github.com>
2019-04-09 15:49:42 +0200

    Update sub_16S.sh

--- metabarcoding/16S/sub_16S.sh
+++ metabarcoding/16S/sub_16S.sh
@@ -8,21 +8,31 @@
 #$ -V
 #$ -cwd
 
-mkdir /homedir/galati/data/16S/16S_sub/
-SUB=/homedir/galati/data/16S/16S_sub/
-TRIM=/homedir/galati/data/16S/16S_primer_trimmed2/
+module purge
+module load bioinfo/vsearch/2.9.1
+
+mkdir /homedir/galati/data/16S_sub/
+SUB=/homedir/galati/data/16S_sub/
+TRIM=/homedir/galati/data/16S_primer_trimmed2/
 cd ${TRIM}
-dir ${TRIM} R*.fastq.gz > filenames.txt
+dir ${TRIM} *.fastq.gz > filenames.txt
 
 echo "Création d'un fichier avec les noms d'échantillons"
 
-for NAME in `awk '{print $1}' ${TRIM}listfile.txt`
+for NAME in `awk '{print $1}' ${TRIM}filenames.txt`
 
 do
 
 echo "Sous-échantillonnage" ${NAME}
 
-vsearch --fastx_subsample ${NAME} --fastqout ${SUB}$NAME --sample_size 1000 --randseed 3
+vsearch --fastx_subsample ${NAME} --fastqout ${SUB}$NAME --sample_size 100 --randseed 3
 
 done
 
+echo "Suppression des fichiers vides"
+find /homedir/galati/data/16S_sub/ -empty -type f -delete
+
+# JOB END
+date
+
+exit 0
