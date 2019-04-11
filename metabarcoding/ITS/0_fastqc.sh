1ba97d740389e0fb4ad8c505d3d3bc6fecdc8a0c
MGalati <46815903+MGalati@users.noreply.github.com>
2019-04-11 17:33:45 +0200

    Update 0_fastqc.sh

--- metabarcoding/ITS/0_fastqc.sh
+++ metabarcoding/ITS/0_fastqc.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#$ -q normal.q
-#$ -N fastqc_ITS
+#$ -q bigmem.q
+#$ -N QC_ITS
 #$ -M mathias.galati@cirad.fr
 #$ -pe parallel_smp 6
 #$ -l mem_free=6G
 #$ -V
 #$ -cwd
-#$ -V
 
 
 module purge
@@ -19,8 +18,8 @@ echo "Début des analyses FastQC/MultiQC"
 # Les fichiers doivent respecter le format Qiime2 :
 # par exemple : L2S357_15_L001_R1_001.fastq.gz
 
-MOCK=/homedir/galati/mock/analysis/ITS/pair/ITS_mock24
-RAW_ITS=/homedir/galati/data/ITS
+MOCK=/homedir/galati/data/metab/ITS/MOCK/ITS_mock24
+RAW_ITS=/homedir/galati/data/metab/ITS/RAW
 
 for f in ${MOCK} ${RAW_ITS}
 do
@@ -30,6 +29,9 @@ multiqc ${f}/qc/ -o ${f}/qc/
 done
 
 echo 'Fin des analyses'
+
+mkdir /homedir/galati/data/metab/ITS/RAW_qc/
+mv /homedir/galati/data/metab/ITS/RAW/qc/* /homedir/galati/data/metab/ITS/RAW_qc/
 
 date
  
