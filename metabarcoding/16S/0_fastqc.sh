91e1637f787bac96010f940e0e894b28bf6fdd07
MGalati <46815903+MGalati@users.noreply.github.com>
2019-04-11 17:31:10 +0200

    Update 0_fastqc.sh

--- metabarcoding/16S/0_fastqc.sh
+++ metabarcoding/16S/0_fastqc.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-#$ -q normal.q
-#$ -N fastqc_16S
+#$ -q bigmem.q
+#$ -N QC_16S
 #$ -M mathias.galati@cirad.fr
 #$ -pe parallel_smp 6
 #$ -l mem_free=6G
 #$ -V
 #$ -cwd
+#$ -V
+
 
 module purge
 module load system/conda/5.1.0
-# conda create --name fastqc_multiqc
-conda activate fastqc_multiqc
+source activate fastqc_multiqc
 
 echo "Début des analyses FastQC/MultiQC"
 
 # Les fichiers doivent respecter le format Qiime2 :
 # par exemple : L2S357_15_L001_R1_001.fastq.gz
 
-MOCK=/homedir/galati/mock/analysis/16S/pair/Mock_S280
-RAW_16S=/homedir/galati/data/16S/
+MOCK=/homedir/galati/data/metab/16S/MOCK/Mock_S280
+RAW_16S=/homedir/galati/data/metab/16S/RAW
 
 for f in ${MOCK} ${RAW_16S}
 do
@@ -29,6 +30,9 @@ multiqc ${f}/qc/ -o ${f}/qc/
 done
 
 echo 'Fin des analyses'
+
+mkdir /homedir/galati/data/metab/16S/RAW_qc/
+mv /homedir/galati/data/metab/16S/RAW/qc/* /homedir/galati/data/metab/16S/RAW_qc/
 
 date
  
