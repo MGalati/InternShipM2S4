5803d5bda159e52f666b18ef9602b1b7fdc75bc8
MGalati <46815903+MGalati@users.noreply.github.com>
2019-04-24 17:27:45 +0200

    Update 2_Svsearch.sh

--- metabarcoding/16S/vsearch/2_Svsearch.sh
+++ metabarcoding/16S/vsearch/2_Svsearch.sh
@@ -21,9 +21,10 @@ IN=/homedir/galati/data/metab/16S
 RUN1=SUB
 RUN2=Mock_S280
 
+echo 'Import'
 for seqs in ${RUN1} ${RUN2}
 do
-qiime tools import --type SampleData[JoinedSequencesWithQuality] \
+qiime tools import --type SampleData[PairedEndSequencesWithQuality] \
                    --input-path ${IN}/${seqs} \
                    --output-path ${IN}/${seqs}_reads.qza \
                    --input-format CasavaOneEightSingleLanePerSampleDirFmt 
@@ -31,41 +32,44 @@ qiime tools import --type SampleData[JoinedSequencesWithQuality] \
 #Check this artifact to make sure that QIIME now recognizes your data
 qiime tools peek ${IN}/${seqs}_reads.qza 
 
-### 'Initial' sequence quality control
+echo 'Initial sequence quality control'
 qiime demux summarize \
   --i-data ${IN}/${seqs}_reads.qza  \
   --o-visualization ${IN}/${seqs}_reads.qzv  \
   --verbose
 done
 
-mkdir vsearch_output
-
+echo 'Début de boucle vsearch'
 for seqs in ${RUN1} ${RUN2}
 do
 
-### Joining reads
-qiime vsearch join-pairs \
-  --i-demultiplexed-seqs ${IN}/${seqs}_reads.qza \
-  --o-joined-sequences ${IN}/${seqs}_demux-joined.qza
-  
-### Summurize joined data with read quality
-qiime demux summarize \
-  --i-data ${IN}/${seqs}_demux-joined.qza \
-  --o-visualization ${IN}/${seqs}_demux-joined.qzv
+mkdir vsearch_output
+mkdir vsearch_output_${seqs}
 
-### Quality filter
+echo 'Quality filter'
 qiime quality-filter q-score \
- --i-demux ${IN}/${seqs}_demux.qza \
+ --i-demux ${IN}/${seqs}_reads.qza \
  --o-filtered-sequences vsearch_output_${seqs}/${seqs}_demux-filtered.qza \
  --o-filter-stats vsearch_output_${seqs}/${seqs}_demux-filter-stats.qza
 
-### Dereplicate sequences
+##Parameter 'demultiplexed_seqs' received an argument of type SampleData[SequencesWithQuality]. An argument of subtype SampleData[PairedEndSequencesWithQuality] is required.
+##echo 'Joining reads'
+##qiime vsearch join-pairs \
+##  --i-demultiplexed-seqs vsearch_output_${seqs}/${seqs}_demux-filtered.qza \
+##  --o-joined-sequences ${IN}/${seqs}_demux-joined.qza
+
+echo 'Summurize joined data with read quality'
+qiime demux summarize \
+  --i-data ${IN}/${seqs}_demux-joined.qza \
+  --o-visualization ${IN}/${seqs}_demux-joined.qzv
+  
+echo 'Dereplicate sequences'
 qiime vsearch dereplicate-sequences \
-  --i-sequences ${IN}/${seqs}_reads.qza \
+  --i-sequences ${IN}/${seqs}_demux-joined.qza \
   --o-dereplicated-table ${IN}/${seqs}_table.qza \
   --o-dereplicated-sequences ${IN}/${seqs}_rep-seqs.qza
 
-### De novo clustering
+echo 'De novo clustering'
 qiime vsearch cluster-features-de-novo \
   --i-table ${IN}/${seqs}_table.qza \
   --i-sequences ${IN}/${seqs}_rep-seqs.qza \
@@ -73,42 +77,40 @@ qiime vsearch cluster-features-de-novo \
   --o-clustered-sequences vsearch_output_${seqs}/${seqs}_rep-seqs-dn-99.qza \
   --p-perc-identity 0.99
 
-#summarize your filtered/ASV table data
+echo 'summarize your filtered/ASV table data'
 qiime tools export --input-path vsearch_output_${seqs}/${seqs}_demux-filter-stats.qza --output-path vsearch_output_${seqs}/${seqs}
 
-qiime feature-table summarize --i-table vsearch_output_${seqs}/${seqs}_table.qza --o-visualization vsearch_output_${seqs}/${seqs}_table_summary.qzv --verbose
+qiime feature-table summarize --i-table ${IN}/${seqs}_table.qza --o-visualization ${IN}/${seqs}_table_summary.qzv --verbose
 
 done
+echo 'Fin de boucle'
+echo 'Merging denoised data'
 
-### Merging denoised data
-
-# ASV table
+echo 'ASV table'
 qiime feature-table merge \
   --i-tables vsearch_output_${RUN1}/${RUN1}_table-dn-99.qza \
   --i-tables vsearch_output_${RUN2}/${RUN2}_table-dn-99.qza \
   --o-merged-table vsearch_output/table.qza
 
-#summarize
+echo 'summarize'
 qiime feature-table summarize \
   --i-table vsearch_output/table.qza \
   --o-visualization vsearch_output/table.qzv 
-  ##--m-sample-metadata-file sample-metadata.tsv
+  #--m-sample-metadata-file sample-metadata.tsv
 
+echo 'Representative sequences'
+qiime feature-table merge-seqs \
+  --i-data vsearch_output_${RUN1}/${RUN1}_rep-seqs-dn-99.qza \
+  --i-data vsearch_output_${RUN2}/${RUN2}_rep-seqs-dn-99.qza \
+  --o-merged-data vsearch_output/representative_sequences.qza
 
-#qiime feature-table summarize \
-#  --i-table table.qza \
-#  --o-visualization table.qzv \
-#  --m-sample-metadata-file sample-metadata.tsv
-#qiime feature-table tabulate-seqs \
-#  --i-data rep-seqs.qza \
-#  --o-visualization rep-seqs.qzv
-
-### Build quick phylogeny with FastTree
-#https://github.com/LangilleLab/microbiome_helper/wiki/Amplicon-SOP-v2-(qiime2-2018.8)
-#https://docs.qiime2.org/2018.11/tutorials/moving-pictures/
+qiime feature-table tabulate-seqs \
+  --i-data dada2_output/representative_sequences.qza\
+  --o-visualization dada2_output/representative_sequences.qzv
 
 mkdir phylogeny
 
+echo 'Phylogenie'
 qiime phylogeny align-to-tree-mafft-fasttree \
   --p-n-threads ${NSLOTS} \
   --i-sequences vsearch_output/representative_sequences.qza \
@@ -122,9 +124,8 @@ qiime phylogeny align-to-tree-mafft-fasttree \
 # loop to test various taxonomic database - pour toi laisser juste silva123 - 
 # https://www.dropbox.com/s/5tckx2vhrmf3flp/silva-132-99-nb-classifier.qza?dl=0
 
-
 mkdir taxonomy
-
+echo 'Classifier'
 qiime feature-classifier classify-sklearn \
   --i-classifier /homedir/galati/data/metab/16S/classifier/silva-132-99-nb-classifier.qza \
   --i-reads vsearch_output/representative_sequences.qza \
@@ -136,16 +137,15 @@ qiime metadata tabulate \
   --m-input-file taxonomy/16S_taxonomy.qza \
   --o-visualization taxonomy/16S_taxonomy.qzv
 
-# necessite metadata
-# qiime taxa barplot \
-#  --i-table vsearch_output/table.qza \
-#  --i-taxonomy taxonomy/${DB}_taxonomy.qza \
-#  --o-visualization taxonomy/${DB}_taxa-bar-plots.qzv \
-#  --m-metadata-file metadata.tsv 
+echo 'necessite metadata'
+ qiime taxa barplot \
+  --i-table vsearch_output/table.qza \
+  --i-taxonomy taxonomy/16S_taxonomy.qza \
+  --o-visualization taxonomy/taxa-bar-plots.qzv \
+  --m-metadata-file /homedir/galati/data/metab/16S/metadata/metadata.tsv
 
 qiime tools export --input-path taxonomy/16S_taxonomy.qza --output-path taxonomy
 mv taxonomy/taxonomy.tsv taxonomy/16S_taxonomy.tsv
-
 
 ### Exporting and modifying BIOM tables
 
@@ -180,12 +180,10 @@ mv export/tree.nwk export/unrooted-tree.nwk
 qiime tools export --input-path phylogeny/rooted-tree.qza --output-path export
 mv export/tree.nwk export/rooted-tree.nwk
 
-
 # For phyloseq you will need :
 #ls export/feature-table.biom.tsv export/taxonomy.tsv export/unrooted-tree.nwk export/rooted-tree.nwk
 
 zip export/export.zip export/* vsearch_outpu*/*qzv taxonomy/*.qzv
-
 
 # JOB END
 date
