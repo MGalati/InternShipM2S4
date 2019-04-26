7d7224ecf6bf243b0291aecb85d2bf530a54dde5
MGalati <46815903+MGalati@users.noreply.github.com>
2019-04-26 14:59:40 +0200

    Update 2_q2_deblur.sh

--- metabarcoding/16S/deblur/2_q2_deblur.sh
+++ metabarcoding/16S/deblur/2_q2_deblur.sh
@@ -3,8 +3,8 @@
 #$ -q bigmem.q
 #$ -N deblur_16S
 #$ -M mathias.galati@cirad.fr
-#$ -pe parallel_smp 10
-#$ -l mem_free=8G
+#$ -pe parallel_smp 15
+#$ -l mem_free=6G
 #$ -V
 #$ -cwd
 
@@ -19,36 +19,51 @@ IN=/homedir/galati/data/metab/16S
 RUN1=PRIM
 RUN2=Mock_S280
 
+#mkdir /homedir/galati/data/metab/16S/deblur
+deblur=/homedir/galati/data/metab/16S/deblur
+"""
 for seqs in ${RUN1} ${RUN2}
 do
+echo 'Import'
 qiime tools import --type SampleData[PairedEndSequencesWithQuality] \
                    --input-path ${IN}/${seqs} \
-                   --output-path ${IN}/${seqs}_reads.qza \
+                   --output-path ${deblur}/${seqs}_reads.qza \
                    --input-format CasavaOneEightSingleLanePerSampleDirFmt 
 
-#Check this artifact to make sure that QIIME now recognizes your data
-qiime tools peek ${IN}/${seqs}_reads.qza 
+echo 'Check this artifact to make sure that QIIME now recognizes your data'
+qiime tools peek ${deblur}/${seqs}_reads.qza 
 
-### 'Initial' sequence quality control
+echo 'Initial sequence quality control'
 qiime demux summarize \
-  --i-data ${IN}/${seqs}_reads.qza  \
-  --o-visualization ${IN}/${seqs}_reads.qzv  \
+  --i-data ${deblur}/${seqs}_reads.qza  \
+  --o-visualization ${deblur}/${seqs}_reads.qzv  \
   --verbose
 done
 
 mkdir deblur_output
+"""
 
 for seqs in ${RUN1} ${RUN2}
 do
 
-### Quality filter
+echo 'Joining reads'
+qiime vsearch join-pairs \
+  --i-demultiplexed-seqs ${deblur}/${seqs}_reads.qza \
+  --o-joined-sequences ${deblur}/${seqs}_reads-joined.qza
 
+echo 'Summary ${seqs}'
+qiime demux summarize \
+  --i-data ${deblur}/${seqs}_reads-joined.qza \
+  --o-visualization ${deblur}/${seqs}_reads-joined.qzv
+
+mkdir deblur_output_${seqs}
+echo 'Quality filter'
 qiime quality-filter q-score-joined \
- --i-demux ${IN}/${seqs}_demux-joined.qza \
+ --i-demux ${deblur}/${seqs}_reads-joined.qza \
  --o-filtered-sequences deblur_output_${seqs}/${seqs}_demux-joined-filtered.qza \
  --o-filter-stats deblur_output_${seqs}/${seqs}_demux-joined-filter-stats.qza
 
-
+echo 'Deblur denoise'
 qiime deblur denoise-16S \
   --i-demultiplexed-seqs deblur_output_${seqs}/${seqs}_demux-joined-filtered.qza \
   --p-trim-length 220 \
@@ -57,77 +72,70 @@ qiime deblur denoise-16S \
   --p-sample-stats \
   --o-stats deblur_output_${seqs}/${seqs}_deblur-stats.qza
 
-
-### Viewing denoising stats
+echo 'Viewing denoising stats'
 qiime metadata tabulate \
   --m-input-file deblur_output_${seqs}/${seqs}_demux-joined-filter-stats.qza \
   --o-visualization deblur_output_${seqs}/${seqs}_demux-joined-filter-stats.qzv
 
-
+echo 'Viualize stats'
 qiime deblur visualize-stats \
   --i-deblur-stats deblur_output_${seqs}/${seqs}_deblur-stats.qza \
   --o-visualization deblur_output_${seqs}/${seqs}_deblur-stats.qzv
 
-#summarize your filtered/ASV table data
-qiime tools export --input-path deblur_output_${seqs}/${seqs}_demux-filter-stats.qza --output-path deblur_output_${seqs}/${seqs}
+echo 'Summarize your filtered/ASV table data'
+qiime tools export --input-path deblur_output_${seqs}/${seqs}_demux-joined-filter-stats.qza --output-path deblur_output_${seqs}/${seqs}
 
+echo 'Summarize your deblur data'
+qiime tools export --input-path deblur_output_${seqs}/${seqs}_deblur-stats.qza --output-path deblur_output_${seqs}/${seqs}
+
+echo 'Feature table summarize'
 qiime feature-table summarize --i-table deblur_output_${seqs}/${seqs}_table-deblur.qza --o-visualization deblur_output_${seqs}/${seqs}_table_summary.qzv --verbose
 
 done
+echo 'Fin de la bouche'
 
-### Merging denoised data
+echo 'Merging denoised data'
 
-# ASV table
+echo 'ASV table'
 qiime feature-table merge \
   --i-tables deblur_output_${RUN1}/${RUN1}_table-deblur.qza \
   --i-tables deblur_output_${RUN2}/${RUN2}_table-deblur.qza \
   --o-merged-table deblur_output/table.qza
 
-# Representative sequences
+echo 'Representative sequences'
 qiime feature-table merge-seqs \
-  --i-data deblur_output_${RUN1}/${RUN1}_representative_sequences.qza \
-  --i-data deblur_output_${RUN2}/${RUN2}_representative_sequences.qza \
+  --i-data deblur_output_${RUN1}/${RUN1}_rep-seqs-deblur.qza \
+  --i-data deblur_output_${RUN2}/${RUN2}_rep-seqs-deblur.qza \
   --o-merged-data deblur_output/representative_sequences.qza
 
-# Denoising Stats
+echo 'Denoising Stats'
 
 cat deblur_output_${RUN1}/${RUN1}/stats.tsv deblur_output_${RUN2}/${RUN2}/stats.tsv \
     > deblur_output/stats.tsv
 
-#cannot
-#qiime feature-table merge \
-#  --i-tables deblur_output/${RUN1}_denoising_stats.qza  \
-#  --i-tables deblur_output/${RUN2}_denoising_stats.qza  \
-#  --o-merged-table deblur_output/denoising_stats.qza
-
-#summarize
+echo 'summarize'
 qiime feature-table summarize \
   --i-table deblur_output/table.qza \
   --o-visualization deblur_output/table.qzv 
-  ##--m-sample-metadata-file sample-metadata.tsv
+  --m-sample-metadata-file /homedir/galati/data/metab/16S/metadata/sample-metadata.tsv
 
 qiime feature-table tabulate-seqs \
   --i-data deblur_output/representative_sequences.qza\
   --o-visualization deblur_output/representative_sequences.qzv
 
-#cannot
-#qiime feature-table summarize --i-table deblur_output/denoising_stats.qza \
-#  --o-visualization deblur_output/denoising_stats.qzv
+echo 'export'
+qiime tools export deblur_output/denoising_stats.qza --output-path deblur_output
 
-#export
-#qiime tools export deblur_output/denoising_stats.qza --output-path deblur_output
+echo 'Summarize'
+qiime feature-table summarize \
+  --i-table table.qza \
+  --o-visualization table.qzv \
+  --m-sample-metadata-file /homedir/galati/data/metab/16S/metadata/sample-metadata.tsv
 
-#qiime feature-table summarize \
-#  --i-table table.qza \
-#  --o-visualization table.qzv \
-#  --m-sample-metadata-file sample-metadata.tsv
-#qiime feature-table tabulate-seqs \
-#  --i-data rep-seqs.qza \
-#  --o-visualization rep-seqs.qzv
-
-### Build quick phylogeny with FastTree
-#https://github.com/LangilleLab/microbiome_helper/wiki/Amplicon-SOP-v2-(qiime2-2018.8)
-#https://docs.qiime2.org/2018.11/tutorials/moving-pictures/
+echo 'Tabulate sequences'
+qiime feature-table tabulate-seqs \
+  --i-data rep-seqs.qza \
+  --o-visualization rep-seqs.qzv
 
 mkdir phylogeny
 
@@ -140,41 +148,33 @@ qiime phylogeny align-to-tree-mafft-fasttree \
   --o-rooted-tree phylogeny/rooted-tree.qza \
   --verbose
 
-#qiime tools export --input-path deblur_output/deblur_table_filt.qza --output-path deblur_output_exported
-#qiime tools export --input-path deblur_output/rep_seqs_filt.qza --output-path deblur_output_exported
-
-### Assign Taxonomy
-# loop to test various taxonomic database - pour toi laisser juste silva123 - 
-# https://www.dropbox.com/s/5tckx2vhrmf3flp/silva-132-99-nb-classifier.qza?dl=0
-
-
-mkdir taxonomy
-
+#mkdir taxonomy
+echo 'clasif'
 qiime feature-classifier classify-sklearn \
   --i-classifier /homedir/galati/data/metab/16S/classifier/silva-132-99-nb-classifier.qza \
   --i-reads deblur_output/representative_sequences.qza \
   --o-classification taxonomy/16S_taxonomy.qza \
   --p-n-jobs ${NSLOTS} \
   --verbose
-
+echo 'tabulate'
 qiime metadata tabulate \
   --m-input-file taxonomy/16S_taxonomy.qza \
   --o-visualization taxonomy/16S_taxonomy.qzv
 
-# necessite metadata
-# qiime taxa barplot \
-#  --i-table deblur_output/table.qza \
-#  --i-taxonomy taxonomy/${DB}_taxonomy.qza \
-#  --o-visualization taxonomy/${DB}_taxa-bar-plots.qzv \
-#  --m-metadata-file metadata.tsv 
+echo 'necessite metadata'
+qiime taxa barplot \
+  --i-table deblur_output/table.qza \
+  --i-taxonomy taxonomy/16S_taxonomy.qza \
+  --o-visualization taxonomy/16S_taxa-bar-plots.qzv \
+  --m-metadata-file /homedir/galati/data/metab/16S/metadata/metadata.tsv 
 
+echo 'export'
 qiime tools export --input-path taxonomy/16S_taxonomy.qza --output-path taxonomy
 mv taxonomy/taxonomy.tsv taxonomy/16S_taxonomy.tsv
 
 
-### Exporting and modifying BIOM tables
-
-#Creating a TSV BIOM table
+echo 'Exporting and modifying BIOM tables'
+echo 'Creating a TSV BIOM table'
 qiime tools export --input-path deblur_output/table.qza --output-path export
 biom convert -i export/feature-table.biom -o export/ASV-table.biom.tsv --to-tsv
 
@@ -188,7 +188,7 @@ sed -i "1d" export/ASV-table.biom.tsv
 
 sed -i "s/#OTU ID/#OTUID/g" export/feature-table.biom.tsv
 
-#Export Taxonomy
+echo 'Export Taxonomy'
 qiime tools export --input-path /homedir/galati/data/metab/16S/classifier/silva-132-99-nb-classifier.qza --output-path export
 
 biom add-metadata -i export/ASV-table.biom.tsv  -o export/ASV-table-silva-132-taxonomy.biom \
@@ -196,10 +196,10 @@ biom add-metadata -i export/ASV-table.biom.tsv  -o export/ASV-table-silva-132-ta
   --sc-separated taxonomy
 biom convert -i export/ASV-table-silva-132-taxonomy.biom -o export/ASV-table-silva-132-taxonomy.biom.tsv --to-tsv
 
-#Export ASV seqs
+echo 'Export ASV seqs'
 qiime tools export --input-path deblur_output/representative_sequences.qza --output-path export
 
-#Export Tree
+echo '#Export Tree'
 qiime tools export --input-path phylogeny/unrooted-tree.qza --output-path export
 mv export/tree.nwk export/unrooted-tree.nwk
 qiime tools export --input-path phylogeny/rooted-tree.qza --output-path export
