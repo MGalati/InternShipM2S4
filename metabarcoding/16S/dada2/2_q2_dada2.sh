b3f2e266181a1af1fa215b1d23f63e1f3ea89fbf
MGalati <46815903+MGalati@users.noreply.github.com>
2019-04-26 14:58:41 +0200

    Update 2_q2_dada2.sh

--- metabarcoding/16S/dada2/2_q2_dada2.sh
+++ metabarcoding/16S/dada2/2_q2_dada2.sh
@@ -3,8 +3,8 @@
 #$ -q bigmem.q
 #$ -N dada2_16S
 #$ -M mathias.galati@cirad.fr
-#$ -pe parallel_smp 10
-#$ -l mem_free=8G
+#$ -pe parallel_smp 15
+#$ -l mem_free=6G
 #$ -V
 #$ -cwd
 
@@ -18,7 +18,8 @@ IN=/homedir/galati/data/metab/16S
 
 RUN1=PRIM
 RUN2=Mock_S280
-
+"""
+echo 'Import sequences'
 for seqs in ${RUN1} ${RUN2}
 do
 qiime tools import --type SampleData[PairedEndSequencesWithQuality] \
@@ -26,21 +27,22 @@ qiime tools import --type SampleData[PairedEndSequencesWithQuality] \
                    --output-path ${IN}/${seqs}_reads.qza \
                    --input-format CasavaOneEightSingleLanePerSampleDirFmt 
 
-#Check this artifact to make sure that QIIME now recognizes your data
+echo 'Check this artifact to make sure that QIIME now recognizes your data'
 qiime tools peek ${IN}/${seqs}_reads.qza 
 
-### 'Initial' sequence quality control
+echo 'Initial sequence quality control'
 qiime demux summarize \
   --i-data ${IN}/${seqs}_reads.qza  \
   --o-visualization ${IN}/${seqs}_reads.qzv  \
   --verbose
 done
-
+"""
+echo 'dada2'
 for seqs in ${RUN1} ${RUN2}
 do
 
-truncF=275 #Il faut Visualiser les graphes pour affiner ces valeurs en fontion des qualités
-truncL=275
+truncF=0 #Il faut Visualiser les graphes pour affiner ces valeurs en fontion des qualités
+truncL=0
 trimF=0
 trimL=0
 maxee=2
@@ -66,34 +68,34 @@ qiime dada2 denoise-paired --i-demultiplexed-seqs ${IN}/${seqs}_reads.qza \
                            --verbose
 #                          --output-dir dada2_output \
 
-### Viewing denoising stats
+echo 'Viewing denoising stats'
 qiime metadata tabulate \
   --m-input-file dada2_output_${seqs}/${seqs}_denoising_stats.qza \
   --o-visualization dada2_output_${seqs}/${seqs}_denoising_stats.qza
 
-#summarize your filtered/ASV table data
+echo 'summarize your filtered/ASV table data'
 qiime tools export --input-path dada2_output_${seqs}/${seqs}_denoising_stats.qza --output-path dada2_output_${seqs}/${seqs}
 
 qiime feature-table summarize --i-table dada2_output_${seqs}/${seqs}_table.qza --o-visualization dada2_output_${seqs}/${seqs}_table_summary.qzv --verbose
 
 done
 
-### Merging denoised data
+echo 'Merging denoised data'
 
 mkdir dada2_output
-# ASV table
+echo 'ASV table'
 qiime feature-table merge \
   --i-tables dada2_output_${RUN1}/${RUN1}_table.qza \
   --i-tables dada2_output_${RUN2}/${RUN2}_table.qza \
   --o-merged-table dada2_output/table.qza
 
-# Representative sequences
+echo 'Representative sequences'
 qiime feature-table merge-seqs \
   --i-data dada2_output_${RUN1}/${RUN1}_representative_sequences.qza \
   --i-data dada2_output_${RUN2}/${RUN2}_representative_sequences.qza \
   --o-merged-data dada2_output/representative_sequences.qza
 
-# Denoising Stats
+echo 'Denoising Stats'
 
 cat dada2_output_${RUN1}/${RUN1}/stats.tsv dada2_output_${RUN2}/${RUN2}/stats.tsv \
     > dada2_output/stats.tsv
@@ -104,11 +106,11 @@ cat dada2_output_${RUN1}/${RUN1}/stats.tsv dada2_output_${RUN2}/${RUN2}/stats.ts
 #  --i-tables dada2_output/${RUN2}_denoising_stats.qza  \
 #  --o-merged-table dada2_output/denoising_stats.qza
 
-#summarize
+echo 'summarize'
 qiime feature-table summarize \
   --i-table dada2_output/table.qza \
   --o-visualization dada2_output/table.qzv 
-  ##--m-sample-metadata-file sample-metadata.tsv
+  --m-sample-metadata-file /homedir/galati/data/metab/16S/metadata/sample-metadata.tsv
 
 qiime feature-table tabulate-seqs \
   --i-data dada2_output/representative_sequences.qza\
@@ -133,6 +135,7 @@ qiime feature-table tabulate-seqs \
 #https://github.com/LangilleLab/microbiome_helper/wiki/Amplicon-SOP-v2-(qiime2-2018.8)
 #https://docs.qiime2.org/2018.11/tutorials/moving-pictures/
 
+echo 'phylogeny'
 mkdir phylogeny
 
 qiime phylogeny align-to-tree-mafft-fasttree \
@@ -147,10 +150,7 @@ qiime phylogeny align-to-tree-mafft-fasttree \
 #qiime tools export --input-path dada2_output/deblur_table_filt.qza --output-path dada2_output_exported
 #qiime tools export --input-path dada2_output/rep_seqs_filt.qza --output-path dada2_output_exported
 
-### Assign Taxonomy
-# loop to test various taxonomic database - pour toi laisser juste silva123 - 
-# https://www.dropbox.com/s/5tckx2vhrmf3flp/silva-132-99-nb-classifier.qza?dl=0
-
+echo 'taxonomy'
 mkdir taxonomy
 
 qiime feature-classifier classify-sklearn \
@@ -165,19 +165,19 @@ qiime metadata tabulate \
   --o-visualization taxonomy/16S_taxonomy.qzv
 
 # necessite metadata
-# qiime taxa barplot \
-#  --i-table dada2_output/table.qza \
-#  --i-taxonomy taxonomy/${DB}_taxonomy.qza \
-#  --o-visualization taxonomy/${DB}_taxa-bar-plots.qzv \
-#  --m-metadata-file metadata.tsv 
+qiime taxa barplot \
+  --i-table dada2_output/table.qza \
+  --i-taxonomy taxonomy/16S_taxonomy.qza \
+  --o-visualization taxonomy/taxa-bar-plots.qzv \
+  --m-metadata-file /homedir/galati/data/metab/16S/metadata/metadata.tsv 
 
 qiime tools export --input-path taxonomy/16S_taxonomy.qza --output-path taxonomy
 mv taxonomy/taxonomy.tsv taxonomy/16S_taxonomy.tsv
 
+echo 'Exporting and modifying BIOM tables'
 
-### Exporting and modifying BIOM tables
-
-#Creating a TSV BIOM table
+echo 'Creating a TSV BIOM table'
+mkdir export
 qiime tools export --input-path dada2_output/table.qza --output-path export
 biom convert -i export/feature-table.biom -o export/ASV-table.biom.tsv --to-tsv
 
@@ -186,23 +186,23 @@ cp export/ASV-table.biom.tsv export/feature-table.biom.tsv
 #https://askubuntu.com/questions/20414/find-and-replace-text-within-a-file-using-commands
 # ASV table for phyloseq
 
-sed -i "s/#OTU ID/OTUID/g" export/ASV-table.biom.tsv
-sed -i "1d" export/ASV-table.biom.tsv
+sed -i 's/#OTU ID/OTUID/g' export/ASV-table.biom.tsv
+sed -i '1d' export/ASV-table.biom.tsv
 
-sed -i "s/#OTU ID/#OTUID/g" export/feature-table.biom.tsv
+sed -i 's/#OTU ID/#OTUID/g' export/feature-table.biom.tsv
 
-#Export Taxonomy
-qiime tools export --input-path /homedir/galati/data/metab/16S/classifier/silva-132-99-nb-classifier.qza --output-path export
+echo 'Export Taxonomy'
+qiime tools export --input-path /homedir/galati/data/metab/16S/classifier/silva-132-99-nb-classifier.qza --output-path export/silva-132_taxonomy.tsv
 
 biom add-metadata -i export/ASV-table.biom.tsv -o export/ASV-table-silva-132-taxonomy.biom \
   --observation-metadata-fp export/silva-132_taxonomy.tsv \
   --sc-separated taxonomy
 biom convert -i export/ASV-table-silva-132-taxonomy.biom -o export/ASV-table-silva-132-taxonomy.biom.tsv --to-tsv
 
-#Export ASV seqs
+echo 'Export ASV seqs'
 qiime tools export --input-path dada2_output/representative_sequences.qza --output-path export
 
-#Export Tree
+echo 'Export Tree'
 qiime tools export --input-path phylogeny/unrooted-tree.qza --output-path export
 mv export/tree.nwk export/unrooted-tree.nwk
 qiime tools export --input-path phylogeny/rooted-tree.qza --output-path export
@@ -210,7 +210,7 @@ mv export/tree.nwk export/rooted-tree.nwk
 
 
 # For phyloseq you will need :
-#ls export/feature-table.biom.tsv export/taxonomy.tsv export/unrooted-tree.nwk export/rooted-tree.nwk
+ls export/feature-table.biom.tsv export/taxonomy.tsv export/unrooted-tree.nwk export/rooted-tree.nwk
 
 zip export/export.zip export/* dada2_outpu*/*qzv taxonomy/*.qzv
 
